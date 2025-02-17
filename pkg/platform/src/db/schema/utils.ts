6a83879d345dd0ee1701ab79abda8b7016f1042d
Zach Kelling <z@zeekay.io>
2025-02-17 14:49:10 -0600

    Clean up

--- pkg/platform/src/db/schema/utils.ts
+++ pkg/platform/src/db/schema/utils.ts
@@ -1,5 +1,5 @@
-import { generatePassword } from "@hanzo/platform/templates/utils";
 import { faker } from "@faker-js/faker";
+import { generatePassword } from "@hanzo/platform/templates/utils";
 import { customAlphabet } from "nanoid";
 
 const alphabet = "abcdefghijklmnopqrstuvwxyz123456789";
