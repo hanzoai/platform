18a44ab79b0d787a06604a5745fcf5e84bde2db1
Zach Kelling <z@zeekay.io>
2025-02-17 20:26:26 -0600

    Update git repo

--- app/dash/docker/build.sh
+++ app/dash/docker/build.sh
@@ -12,6 +12,6 @@ fi
 
 BUILDER=$(docker buildx create --use)
 
-docker buildx build --platform linux/amd64,linux/arm64 --pull --rm -t "hanzo/hanzo:${TAG}" -f 'Dockerfile' .
+docker buildx build --platform linux/amd64,linux/arm64 --pull --rm -t "hanzoai/platform:${TAG}" -f 'Dockerfile' .
 
 docker buildx rm $BUILDER
