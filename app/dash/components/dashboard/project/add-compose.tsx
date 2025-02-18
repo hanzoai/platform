e499f7ebe98da6f940b74e77aa0a31b3da3c395b
Zach Kelling <z@zeekay.io>
2025-02-17 18:34:54 -0600

    Update Docs

--- app/dash/components/dashboard/project/add-compose.tsx
+++ app/dash/components/dashboard/project/add-compose.tsx
@@ -251,7 +251,7 @@ export const AddCompose = ({ projectId, projectName }: Props) => {
 										</FormControl>
 										<SelectContent>
 											<SelectItem value="docker-compose">
-												Docker Compose
+												Kompose
 											</SelectItem>
 											<SelectItem value="stack">Stack</SelectItem>
 										</SelectContent>
