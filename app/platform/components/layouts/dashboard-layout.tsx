b98661c1904ed5e2f1e4106c5b935a1f03edb0f1
Zach Kelling <z@zeekay.io>
2025-02-27 05:32:44 +0000

    Add Analytics and Base

--- app/platform/components/layouts/dashboard-layout.tsx
+++
@@ -1,9 +0,0 @@
-import Page from "./side";
-interface Props {
-	children: React.ReactNode;
-	metaName?: string;
-}
-
-export const DashboardLayout = ({ children }: Props) => {
-	return <Page>{children}</Page>;
-};
