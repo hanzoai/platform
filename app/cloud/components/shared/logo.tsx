05094726a3bf62a51fe022c5a7f50699c6481af9
Zach Kelling <z@zeekay.io>
2025-02-28 12:16:31 +0000

    Shrink logos

--- app/cloud/components/shared/logo.tsx
+++ app/cloud/components/shared/logo.tsx
@@ -4,7 +4,7 @@ interface Props {
 	className?: string;
 }
 
-export const Logo = ({ className = "size-14" }: Props) => {
+export const Logo = ({ className = "size-7" }: Props) => {
 	return (
 		<svg
 			xmlns="http://www.w3.org/2000/svg"
