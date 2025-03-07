b3d8795982ed719c777683455a0a3a661e69f05b
Zach Kelling <z@zeekay.io>
2025-03-07 09:42:54 +0000

    Enable Redis to be overriden

--- app/hanzo/server/queues/redis-connection.ts
+++ app/hanzo/server/queues/redis-connection.ts
@@ -1,5 +1,5 @@
 import type { ConnectionOptions } from "bullmq";
 
-export const redisConfig: ConnectionOptions = {
-	host: process.env.NODE_ENV === "production" ? "hanzo-redis" : "127.0.0.1",
-};
+export const redisConfig: ConnectionOptions = process.env.REDIS_URL
+  ? { url: process.env.REDIS_URL }
+  : { host: process.env.REDIS_HOST || (process.env.NODE_ENV === "production" ? "hanzo-redis" : "127.0.0.1") };
