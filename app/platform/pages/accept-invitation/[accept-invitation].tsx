dd83e6e7286dbe1da513536a7cf5c8999b4b52ec
Zach Kelling <z@zeekay.io>
2026-01-26 18:22:39 -0800

    fix: add getServerSideProps to accept-invitation page to prevent SSG

--- app/platform/pages/accept-invitation/[accept-invitation].tsx
+++ app/platform/pages/accept-invitation/[accept-invitation].tsx
@@ -1,15 +1,12 @@
 import { useRouter } from "next/router";
 import { Button } from "@/components/ui/button";
 import { authClient } from "@/lib/auth-client";
+import type { GetServerSidePropsContext } from "next";
 
 export const AcceptInvitation = () => {
 	const { query } = useRouter();
 
 	const invitationId = query["accept-invitation"];
-
-	// const { data: organization } = api.organization.getById.useQuery({
-	//     id: id as string
-	// })
 
 	return (
 		<div>
@@ -26,5 +23,10 @@ export const AcceptInvitation = () => {
 		</div>
 	);
 };
+
+// Force server-side rendering to avoid static generation
+export async function getServerSideProps(ctx: GetServerSidePropsContext) {
+	return { props: {} };
+}
 
 export default AcceptInvitation;
