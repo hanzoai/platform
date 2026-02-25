a845a198210b3a28a3dc450f905756025c36e1f1
Mauricio Siu <siumauricio@icloud.com>
2026-02-24 19:01:44 -0600

    refactor: reorganize imports in utils.ts to enhance clarity and maintainability

--- apps/schedules/src/utils.ts
+++ apps/schedules/src/utils.ts
@@ -15,14 +15,13 @@ import {
 	runVolumeBackup,
 } from "@dokploy/server";
 import {
-	and,
 	backups,
 	db,
-	eq,
 	schedules,
 	server,
 	volumeBackups,
 } from "@dokploy/server/db";
+import { and, eq } from "drizzle-orm";
 import { logger } from "./logger.js";
 import { scheduleJob } from "./queue.js";
 import type { QueueJob } from "./schema.js";
