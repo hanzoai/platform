78e65492753a270d4a592fdeb371790fa4712249
Hanzo Dev <dev@hanzo.ai>
2026-03-03 06:08:36 -0800

    stripe bill fix

--- app/hanzo/server/utils/stripe.ts
+++ app/hanzo/server/utils/stripe.ts
@@ -7,19 +7,32 @@ const BASE_PRICE_MONTHLY_ID = process.env.BASE_PRICE_MONTHLY_ID!; // $4.00
 
 const BASE_ANNUAL_MONTHLY_ID = process.env.BASE_ANNUAL_MONTHLY_ID!; // $7.99
 
-export const getStripeItems = (serverQuantity: number, isAnnual: boolean) => {
+export const getStripeItems = (serverQuantity: number, isAnnual: boolean, priceId: string) => {
 	const items = [];
+
+	// if (isAnnual) {
+	// 	items.push({
+	// 		price: BASE_ANNUAL_MONTHLY_ID,
+	// 		quantity: serverQuantity,
+	// 	});
+
+	// 	return items;
+	// }
+	// items.push({
+	// 	price: BASE_PRICE_MONTHLY_ID,
+	// 	quantity: serverQuantity,
+	// });
 
 	if (isAnnual) {
 		items.push({
-			price: BASE_ANNUAL_MONTHLY_ID,
+			price: priceId,
 			quantity: serverQuantity,
 		});
 
 		return items;
 	}
 	items.push({
-		price: BASE_PRICE_MONTHLY_ID,
+		price: priceId,
 		quantity: serverQuantity,
 	});
 
