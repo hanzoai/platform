ca17252b9a42f585b8f2444c675367b8832e7179
Your Name <you@example.com>
2026-03-03 06:35:12 -0800

    Add Docker Compose Spec support and improve swarm functionality

--- pkg/core/src/index.ts
+++ pkg/core/src/index.ts
@@ -1,119 +1,53 @@
-export * from "./auth/random-password";
-export * from "./services/admin";
-export * from "./services/user";
-export * from "./services/project";
+// Environment variables
+export * from "./env";
+
+// Authentication
+export * from "./lib/auth";
+
+// Services
+export * from "./services/application";
+export * from "./services/redis";
+export * from "./services/mount";
 export * from "./services/postgres";
-export * from "./services/domain";
-export * from "./services/mariadb";
-export * from "./services/mongo";
 export * from "./services/mysql";
+export * from "./services/mongo";
+export * from "./services/mariadb";
 export * from "./services/backup";
-export * from "./services/cluster";
+export * from "./services/security";
+export * from "./services/notification";
 export * from "./services/settings";
 export * from "./services/docker";
-export * from "./services/destination";
-export * from "./services/deployment";
-export * from "./services/mount";
 export * from "./services/certificate";
-export * from "./services/redirect";
-export * from "./services/security";
-// Removed preview-deployment
-export * from "./services/port";
-export * from "./services/redis";
-// Removed compose service
+export * from "./services/cluster";
+export * from "./services/destination";
+export * from "./services/domain";
+export * from "./services/project";
 export * from "./services/registry";
-export * from "./services/notification";
+export * from "./services/port";
 export * from "./services/ssh-key";
+export * from "./services/user";
+export * from "./services/ai";
+export * from "./services/deployment";
 export * from "./services/git-provider";
-export * from "./services/bitbucket";
-export * from "./services/github";
 export * from "./services/gitlab";
+export * from "./services/github";
+export * from "./services/bitbucket";
+export * from "./services/admin";
 export * from "./services/server";
-export * from "./services/application";
 
+// Export Compose Spec related functionality
+export * from "./services/compose";
+
+// Utility functions
+export * from "./utils/scheduling/backup";
+export { rebuildDatabase } from "./utils/databases/rebuild";
+
+// Setup
 export * from "./setup/config-paths";
-export * from "./setup/postgres-setup";
-export * from "./setup/redis-setup";
-export * from "./setup/server-setup";
-export * from "./setup/monitoring-setup";
 export * from "./setup/setup";
-export * from "./setup/traefik-setup";
-export * from "./setup/server-validate";
-export * from "./setup/server-audit";
-
-export * from "./utils/backups/index";
-export * from "./utils/backups/mariadb";
-export * from "./utils/backups/mongo";
-export * from "./utils/backups/mysql";
-export * from "./utils/backups/postgres";
-export * from "./utils/backups/utils";
-export * from "./utils/databases/rebuild";
-
-export * from "./utils/notifications/build-error";
-export * from "./utils/notifications/build-success";
-export * from "./utils/notifications/database-backup";
-export * from "./utils/notifications/hanzo-restart";
-export * from "./utils/notifications/utils";
-export * from "./utils/notifications/docker-cleanup";
-export * from "./utils/notifications/server-threshold";
-
-export * from "./utils/builders/index";
-// Removed builders/compose
-export * from "./utils/builders/docker-file";
-export * from "./utils/builders/drop";
-export * from "./utils/builders/heroku";
-export * from "./utils/builders/nixpacks";
-export * from "./utils/builders/paketo";
-export * from "./utils/builders/static";
-export * from "./utils/builders/utils";
-
-export * from "./utils/cluster/upload";
-
-// Removed docker/compose
-export * from "./utils/docker/collision";
-export * from "./utils/docker/domain";
-export * from "./utils/docker/utils";
-export * from "./utils/docker/types";
-// Removed docker/compose/configs
-// Removed docker/compose/network
-// Removed docker/compose/secrets
-// Removed docker/compose/service
-// Removed docker/compose/volume
-
-export * from "./utils/filesystem/directory";
-export * from "./utils/filesystem/ssh";
-
-export * from "./utils/process/execAsync";
-export * from "./utils/process/spawnAsync";
-export * from "./utils/providers/bitbucket";
-export * from "./utils/providers/docker";
-export * from "./utils/providers/git";
-export * from "./utils/providers/github";
-export * from "./utils/providers/gitlab";
-export * from "./utils/providers/raw";
-
-export * from "./utils/servers/remote-docker";
-
-export * from "./utils/traefik/application";
-export * from "./utils/traefik/domain";
-export * from "./utils/traefik/file-types";
-export * from "./utils/traefik/middleware";
-export * from "./utils/traefik/redirect";
-export * from "./utils/traefik/security";
-export * from "./utils/traefik/types";
-export * from "./utils/traefik/web-server";
-
-export * from "./wss/utils";
-
-export * from "./utils/access-log/handler";
-export * from "./utils/access-log/types";
-export * from "./utils/access-log/utils";
-export * from "./constants/index";
-
-export * from "./monitoring/utils";
-
-export * from "./db/validations/domain";
-export * from "./db/validations/index";
-export * from "./utils/gpu-setup";
-
-export * from "./lib/auth";
+// Don't export these to avoid conflicts
+// export * from "./setup/tls-setup";
+// export * from "./setup/traefik-setup";
+export * from "./setup/monitoring-setup";
+export * from "./setup/docker-compose";
+export * from "./setup/docker-swarm";
