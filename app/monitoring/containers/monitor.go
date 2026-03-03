0ace680df2b0771e47542fa512e27d0ec1139610
Zach Kelling <z@zeekay.io>
2026-03-03 00:38:00 -0800

    Update go imports

--- app/monitoring/containers/monitor.go
+++ app/monitoring/containers/monitor.go
@@ -10,8 +10,8 @@ import (
 	"sync"
 	"time"
 
-	"github.com/mauriciogm/hanzo/app/monitoring/config"
-	"github.com/mauriciogm/hanzo/app/monitoring/database"
+	"github.com/hanzoai/hanzo/app/monitoring/config"
+	"github.com/hanzoai/hanzo/app/monitoring/database"
 )
 
 type ContainerMonitor struct {
