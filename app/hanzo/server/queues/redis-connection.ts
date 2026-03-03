7f92d88ec356642d2d48398d79a57662d06979f5
Zach Kelling <z@zeekay.io>
2026-03-03 05:57:12 -0800

    Make REDIS configurable

--- app/hanzo/server/queues/redis-connection.ts
+++ app/hanzo/server/queues/redis-connection.ts
@@ -1,5 +1,9 @@
 import type { ConnectionOptions } from "bullmq";
 
 export const redisConfig: ConnectionOptions = process.env.REDIS_URL
-  ? { url: process.env.REDIS_URL }
-  : { host: process.env.REDIS_HOST || (process.env.NODE_ENV === "production" ? "hanzo-redis" : "127.0.0.1") };
+	? { url: process.env.REDIS_URL }
+	: {
+			host:
+				process.env.REDIS_HOST ||
+				(process.env.NODE_ENV === "production" ? "hanzo-redis" : "127.0.0.1"),
+		};
