81dae3d5e6503d27491eb54760d3d638ac8070f2
Zach Kelling <z@zeekay.io>
2026-03-03 00:11:24 -0800

    Rebrand platform

---
+++ pkg/platform/src/utils/backups/mysql.ts
@@ -0,0 +1,63 @@
+import path from "node:path";
+import type { BackupSchedule } from "@hanzo/platform/services/backup";
+import type { MySql } from "@hanzo/platform/services/mysql";
+import { findProjectById } from "@hanzo/platform/services/project";
+import {
+	getRemoteServiceContainer,
+	getServiceContainer,
+} from "../docker/utils";
+import { sendDatabaseBackupNotifications } from "../notifications/database-backup";
+import { execAsync, execAsyncRemote } from "../process/execAsync";
+import { getS3Credentials } from "./utils";
+
+export const runMySqlBackup = async (mysql: MySql, backup: BackupSchedule) => {
+	const { appName, databaseRootPassword, projectId, name } = mysql;
+	const project = await findProjectById(projectId);
+	const { prefix, database } = backup;
+	const destination = backup.destination;
+	const backupFileName = `${new Date().toISOString()}.sql.gz`;
+	const bucketDestination = path.join(prefix, backupFileName);
+
+	try {
+		const rcloneFlags = getS3Credentials(destination);
+		const rcloneDestination = `:s3:${destination.bucket}/${bucketDestination}`;
+
+		const rcloneCommand = `rclone rcat ${rcloneFlags.join(" ")} "${rcloneDestination}"`;
+		if (mysql.serverId) {
+			const { Id: containerId } = await getRemoteServiceContainer(
+				mysql.serverId,
+				appName,
+			);
+			const mysqlDumpCommand = `docker exec ${containerId} sh -c "mysqldump --default-character-set=utf8mb4 -u 'root' --password='${databaseRootPassword}' --single-transaction --no-tablespaces --quick '${database}' | gzip"`;
+
+			await execAsyncRemote(
+				mysql.serverId,
+				`${mysqlDumpCommand} | ${rcloneCommand}`,
+			);
+		} else {
+			const { Id: containerId } = await getServiceContainer(appName);
+			const mysqlDumpCommand = `docker exec ${containerId} sh -c "mysqldump --default-character-set=utf8mb4 -u 'root' --password='${databaseRootPassword}' --single-transaction --no-tablespaces --quick '${database}' | gzip"`;
+
+			await execAsync(`${mysqlDumpCommand} | ${rcloneCommand}`);
+		}
+		await sendDatabaseBackupNotifications({
+			applicationName: name,
+			projectName: project.name,
+			databaseType: "mysql",
+			type: "success",
+			organizationId: project.organizationId,
+		});
+	} catch (error) {
+		console.log(error);
+		await sendDatabaseBackupNotifications({
+			applicationName: name,
+			projectName: project.name,
+			databaseType: "mysql",
+			type: "error",
+			// @ts-ignore
+			errorMessage: error?.message || "Error message not provided",
+			organizationId: project.organizationId,
+		});
+		throw error;
+	}
+};
