125cc52f4d3cb4fd5b3df7b30cd1ee5be01e8338
Your Name <you@example.com>
2026-03-03 04:37:24 -0800

    Add AI, organizations and other updates.
    Notable changes:
    
    @hanzo/platform is now @hanzo/core.
    
    hanzo is now the main app.

--- app/cloud/server/db/schema/index.ts
+++
@@ -1,1 +0,0 @@
-export * from "@hanzo/platform/db/schema";
---
+++ app/hanzo/reset-password.ts
@@ -0,0 +1,31 @@
+import { findAdmin } from "@hanzo/core";
+import { generateRandomPassword } from "@hanzo/core";
+import { db } from "@hanzo/core/db";
+import { account } from "@hanzo/core/db/schema";
+import { eq } from "drizzle-orm";
+
+(async () => {
+	try {
+		const randomPassword = await generateRandomPassword();
+
+		const result = await findAdmin();
+
+		const update = await db
+			.update(account)
+			.set({
+				password: randomPassword.hashedPassword,
+			})
+			.where(eq(account.userId, result.userId));
+
+		if (update) {
+			console.log("Password reset successful");
+			console.log("New password: ", randomPassword.randomPassword);
+		} else {
+			console.log("Password reset failed");
+		}
+
+		process.exit(0);
+	} catch (error) {
+		console.log("Error resetting password", error);
+	}
+})();
